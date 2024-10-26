5a853a5ae4dd33f781f041546b834fd95a2d1bfa
jahangirmammadli <memmedli0014@gmail.com>
2024-10-26 20:34:06 +0400

    second commit

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,15 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-
-        System.out.println("Hello");
+        Scanner sc = new Scanner(System.in);
+        int a =sc.nextInt();
+        boolean isPositive =checkPositiveInteger(a);
+        System.out.println(isPositive);
+    }
+    static boolean checkPositiveInteger(int num){
+        return num >0;
     }
 }
